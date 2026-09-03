bc60c1c45ffa93715c5432d5e06542c01885c6ab
antoniobensisiqueira-cloud <antonio.bensi.siqueira@escola.pr.gov.br>
2026-09-03 08:03:24 -0300

    Add files via upload

---
+++ atividade 2/scrypt.js
@@ -0,0 +1,10 @@
+function ola(){
+alert("sei la mano voce que clicou");
+}
+function eudisse(){
+    alert("eu disse que nao tinha nada");
+}
+function aleatorio(){
+    let numeroAleatorio = Math.random();
+    alert("o numero é" + numeroAleatorio);
+}
