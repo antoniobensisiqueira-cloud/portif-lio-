e66b05ef0568354a1838ed4572725e7536fd5941
antoniobensisiqueira-cloud <antonio.bensi.siqueira@escola.pr.gov.br>
2026-09-24 09:34:06 -0300

    Add files via upload
    update 1.0

--- atividade 3/script.js
+++ atividade 3/script.js
@@ -6,6 +6,13 @@ function calcular(){
  nota1tri = Number( prompt("digite a nota do primeiro trimestre"));
 nota2tri = Number( prompt("digite a nota do segundo trimestre"));
 
-resultado = 180 - nota1tri + nota2tri;
+resultado = 180 - (nota1tri + nota2tri);
+
+if(resultado <= 0){
+    alert("parabéns você esta aprovado.");
+    
+}else{
+    alert("você ainda precisa" + resultado + "no terceiro tirimestre");
+}
 
 }
